7c9ed5265ba016d39bf0bf38366b7258163247f0
Jacob <jacob.fintan.mitchell.274@gmail.com>
2021-11-19 11:37:58 +0000

    Serial Test

--- TestCode/SerialTest.py
+++ TestCode/SerialTest.py
@@ -3,7 +3,7 @@ import serial
 
 
 if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    ser = serial.Serial('/dev/ttyACM1', 9600, timeout=1)
     ser.reset_input_buffer()
     while True:
         if ser.in_waiting > 0:
